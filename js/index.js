e96f2c34248d05008ca89e18b92cbe57c49ec14c
rossVerenich <rostyslav.verenich@gmail.com>
2015-08-11 02:32:31 +0300

    initial commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$('document').ready(function() {
+	var options = { videoId: 'f-UGhWj1xww', start: 1 };
+	$('#wrapper').tubular(options);
+	// f-UGhWj1xww cool sepia hd
+	// 49SKbS7Xwf4 beautiful barn sepia
+});
